6609c5c70d39ae0f4ab59541f1c56b8bc4c506a7
zedparman <ze.dparman@gmail.com>
2022-02-12 21:07:15 +0330

    Create default.vue

---
+++ layouts/default.vue
@@ -0,0 +1,30 @@
+  <template>
+  <div>
+    <nav class="navbar navbar-expand-xl navbar-dark bg-primary">
+      <Nuxt-link to="/" class="navbar-brand">Nuxt</Nuxt-link>
+      <div id="collapsibleNavId">
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+          <li class="nav-item active">
+            <nuxt-link to="/doctorregister" class="text-white"
+              >doctorregister</nuxt-link
+            >
+            <nuxt-link to="/doctorVerify" class="text-white"
+              >doctorVerify</nuxt-link
+            >
+            <nuxt-link to="/doctorClinic" class="text-white"
+              >doctorClinic</nuxt-link
+            >
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style >
+</style>
